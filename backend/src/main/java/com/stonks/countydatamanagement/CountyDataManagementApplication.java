6bb4d9426f328cf8f0c782af526b5a4767b4557d
GuhTiagoSilva <46786490+GuhTiagoSilva@users.noreply.github.com>
2021-05-24 15:28:03 -0300

    Initial Commit

---
+++ backend/src/main/java/com/stonks/countydatamanagement/CountyDataManagementApplication.java
@@ -0,0 +1,13 @@
+package com.stonks.countydatamanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountyDataManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountyDataManagementApplication.class, args);
+	}
+
+}
