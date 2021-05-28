4bb045cf4517198b68efb4e051e8de3091319222
GuhTiagoSilva <46786490+GuhTiagoSilva@users.noreply.github.com>
2021-05-27 22:17:57 -0300

    Removing unused imports

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import DataTable from './core/components/DataTable';
 import Navbar from './core/components/Navbar';
 import Home from './pages/Home';
 import IncomeList from './pages/IncomeList';
